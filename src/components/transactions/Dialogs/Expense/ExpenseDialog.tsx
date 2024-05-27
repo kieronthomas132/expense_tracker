5380d6e5854e98d896bb687e00eca284652e2c59
kieronthomas <kieron.thomas321@hotmail.co.uk>
2024-05-27 20:03:37 +0100

    updated ExpenseDialog.tsx

--- src/components/transactions/Dialogs/Expense/ExpenseDialog.tsx
+++ src/components/transactions/Dialogs/Expense/ExpenseDialog.tsx
@@ -1,13 +1,11 @@
 import { FormEvent, useEffect } from "react";
 import { useParams } from "react-router";
-import {useAddTransaction} from "@/lib/react-query/queries&Mutations.tsx";
+import { useAddTransaction } from "@/lib/react-query/queries&Mutations.tsx";
 import DialogLayout from "@/components/dialogLayout/DialogLayout.tsx";
 import { IDialog } from "@/components/interfaces/interfaces.tsx";
 import { UserProps, useUserStore } from "@/zustand/UserStore.tsx";
 import { useGetWalletHook } from "@/components/hooks/WalletHooks/getWalletHook.tsx";
-import {handleTransaction} from "@/components/transactions/Functions.tsx";
-import {appwriteConfig, storage} from "@/lib/appwrite/config.tsx";
-import {ID} from "appwrite";
+import {handleTransaction, uploadIcon} from "@/components/transactions/Functions.tsx";
 
 const ExpenseDialog = ({
   handleInputChange,
@@ -29,28 +27,12 @@ const ExpenseDialog = ({
 
   const { amount, description, category, icon } = inputs;
 
-  const uploadIcon = async () => {
-    // Fetch the image from the provided URL
-    const response = await fetch(icon);
-    const blob = await response.blob();
-    const file = new File([blob], 'icon.png', { type: "image/png" });
-
-    // Upload the file to Appwrite Storage
-    const result = await storage.createFile(
-        appwriteConfig.STORAGE_COLLECTION_ID,
-        ID.unique(),
-        file
-    );
-    return `https://cloud.appwrite.io/v1/storage/buckets/${appwriteConfig.STORAGE_COLLECTION_ID}/files/${result["$id"]}/view?project=663bc860002b4e15d7fe&mode=admin`
-  }
-
   const handleAddTransaction = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-
     let iconUrl = icon;
     if (icon) {
-      iconUrl = await uploadIcon();
+      iconUrl = await uploadIcon(icon);
     }
 
     const handleAddExpense = await handleTransaction({
